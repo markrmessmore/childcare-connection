d33501bdb9cc2fd2f2d7af44eadc7ce3ba898701
Mark <markrmessmore@hotmail.com>
2019-02-27 19:32:53 -0500

    Hackathon

--- src/components/sub-components/familyInfo.vue
+++ src/components/sub-components/familyInfo.vue
@@ -1,11 +1,56 @@
 <template lang="html">
   <v-card>
-    Family Info
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs5>
+          <v-text-field
+            name="Applicant Name"
+            label="Applicant Name"
+            v-model="applicantName"
+            box
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs5 offset-xs1>
+          <v-text-field
+            name="Co-applicant Name"
+            label="Co-applicant Name"
+            v-model="coapplicantName"
+            box
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs5>
+          <v-text-field
+            name="password"
+            label="password"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            :append-icon="show1 ? 'visibility_off' : 'visibility'"
+            @click:append="show1 = !show1"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs2 offset-xs10>
+          <v-btn color="primary" @click="saveData()">Save</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data(){
+    return {
+      applicantName: "",
+      rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters'
+      },
+      show1: false,
+    }
+  }
+}
 </script>
 
 <style lang="css">
