25edb65467eb33d6981b05fb6559cd80d186ac80
wbconnor <wmconnor1986@gmail.com>
2019-02-27 20:02:47 -0500

    Begin building out the provider info form

--- src/components/sub-components/providerInfo.vue
+++ src/components/sub-components/providerInfo.vue
@@ -1,6 +1,135 @@
 <template lang="html">
   <v-card>
-    Provider Info
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs5>
+          <v-text-field
+            name="name"
+            label="Name"
+            v-model="name"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5 offset-xs1>
+          <v-text-field
+            name="address"
+            label="Address"
+            v-model="address"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5>
+          <v-text-field
+            name="federalId"
+            label="Federal ID"
+            v-model="federalId"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5  offset-xs1>
+          <v-text-field
+            name="address2"
+            label="Address 2"
+            v-model="address2"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5>
+          <v-text-field
+            name="licenseNum"
+            label="License Number"
+            v-model="licenseNum"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5 offset-xs1>
+          <v-text-field
+            name="city"
+            label="City"
+            v-model="city"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5>
+          <v-text-field
+            name="typeOfProvider"
+            label="Provider Type"
+            v-model="typeOfProvider"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5 offset-xs1>
+          <v-text-field
+            name="state"
+            label="State"
+            v-model="state"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5>
+          <v-text-field
+            name="typeOfCare"
+            label="Care Type"
+            v-model="typeOfCare"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5 offset-xs1>
+          <v-text-field
+            name="zip"
+            label="Zip"
+            v-model="zip"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5>
+          <v-text-field
+            name="monthlyAmt"
+            label="Monthly Amount"
+            v-model="monthlyAmt"
+            box
+          ></v-text-field>
+          </v-flex>
+
+        <v-flex xs5 offset-xs1>
+          <v-text-field
+            name="phone"
+            label="Phone"
+            v-model="phone"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5>
+          <v-text-field
+            name="papaStart"
+            label="papaStart"
+            v-model="papaStart"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs5 offset-xs1>
+          <v-text-field
+            name="papaEnd"
+            label="papaEnd"
+            v-model="papaEnd"
+            box
+          ></v-text-field>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
   </v-card>
 </template>
 
@@ -19,4 +148,20 @@ Add ability to input multiple records capturing the following information:
 * provider phone number
 * Child name (from family info) using this provider
 * Amount designated
+
+name              : "ABC Childcare",
+federalId         : "11111-11",
+licenseNum        : "11111111",
+typeOfProvider    : "Licensed Center",    //COULD BE LICENSED CENTER OR REGISTERED FAMILY CHILD CARE
+typeOfCare        : ["Full Day"],         //COULD SELECT MULTIPLE: FULL DAY, BEFORE SCHOOL, AFTER SCHOOL, SUMMER CAMP
+address           : "987 6th St.",
+address2          : "Suite 201",
+city              : "Trenton",
+state             : "NJ",
+zip               : "08638",
+phone             : "987-654-3210",
+papaStart         : "2/1/2019",
+papaEnd           : "1/31/2019",
+monthlyAmt        : 300,
+
 -->
