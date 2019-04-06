7ab0234dbb9ca8ec2a49ac0080723db18903a0c8
Mark <markrmessmore@hotmail.com>
2019-04-05 22:33:02 -0400

    Began working on report component.

--- src/components/reports.vue
+++ src/components/reports.vue
@@ -7,19 +7,46 @@
       </v-toolbar-title>
     </v-toolbar>
     <v-card-text>
+      <v-layout row wrap align-center justify-space-around>
+        <v-btn color="primary" outline round @click="generateReport('quick')">text</v-btn>
+      </v-layout>
+      <br>
+      <v-card v-if="reportSelected" flat>
+        <quick>
+          <template v-slot:download>
+            <download @close="closeReport()" @download="downloadReport()"></download>
+          </template>
+        </quick>
+      </v-card>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import download from '@/components/sub-components/reports/downloadBar.vue'
+import quick from '@/components/sub-components/reports/quick.vue'
 export default {
+  components: {
+    download, quick
+  },
   data(){
     return{
-
+      reportSelected: false,
+    }
+  },
+  methods: {
+    closeReport(){
+      this.reportSelected = false
+    },
+    downloadReport(){
+      console.log('download')
+    },
+    generateReport(type){
+      this.reportSelected = true
     }
   },
   computed: {
-    
+
   }
 }
 </script>
---
+++ src/components/sub-components/reports/downloadBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-toolbar color="info" dense flat>
+    <v-btn color="secondary" round outline @click="download()">
+      <v-icon left>fas fa-file-download</v-icon>
+        Download Report
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn color="secondary" icon outline @click="closeReport()">
+      <v-icon>close</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeReport(){
+      this.$emit('close')
+    },
+    download(){
+      this.$emit('download')
+    }
+  }
+}
+</script>
