034daeb4e52c9251417bbdd1ca2e339b1a0041c7
Mark <mark@markmessmore.us>
2020-10-05 17:43:48 -0400

    Removed the firebase-dev.js settings from the .gitignore as it was accidentally placed there.

---
+++ src/firebase-dev.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyCF9G2SNzv6vL68XbereiSLUP8ttMTzbpw",
+  authDomain: "ccc-mercer-co-test.firebaseapp.com",
+  databaseURL: "https://ccc-mercer-co-test.firebaseio.com",
+  projectId: "ccc-mercer-co-test",
+  storageBucket: "ccc-mercer-co-test.appspot.com",
+  // messagingSenderId: "167344618504",
+  // appId: "1:167344618504:web:4360abd074a304ae"
+};
